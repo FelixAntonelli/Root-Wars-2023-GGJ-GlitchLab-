cc1906a26bdc21c84b444ea8235d13f3e0e5e8c6
CHollingworth <72136789+CHollingworth@users.noreply.github.com>
2023-02-05 12:42:30 +0000

    Fmod Fix & Main Menu Rev 1

--- 2023-GGJ-GlitchLab/Assets/Scripts/MainMenuManager.cs
+++ 2023-GGJ-GlitchLab/Assets/Scripts/MainMenuManager.cs
@@ -8,21 +8,50 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     public Vector3 EndPostition = new Vector3(0, 0, 0);
     public bool move = false;
+    public bool loadup = false;
     public GameObject mainMenu;
     public void StartGame()
     {
         move = true;
-        mainMenu.SetActive(false);
+       // mainMenu.SetActive(false);
     }
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsync());
+    }
+
     public float speed;
     void Update()
     {
         if (!move) return;
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, EndPostition, step);
+
         if (transform.position == EndPostition)
         {
-            SceneManager.LoadScene("SampleScene");
+            move = false;
+            loadup = true;
         }
     }
+
+    IEnumerator LoadAsync()
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (loadup)
+            {
+                asyncLoad.allowSceneActivation = true;
+                loadup = false;
+                yield return null;
+            }
+            yield return null;
+        }
+
+    }
+
 }
