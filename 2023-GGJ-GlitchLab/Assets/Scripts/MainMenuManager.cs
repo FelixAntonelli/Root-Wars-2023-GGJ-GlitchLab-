82cfe176ea4c6a4350c0c34095874ec93077e315
CHollingworth <72136789+CHollingworth@users.noreply.github.com>
2023-02-05 11:21:00 +0000

    Fmod Additions that were mistakenly stashed

--- 2023-GGJ-GlitchLab/Assets/Scripts/MainMenuManager.cs
+++ 2023-GGJ-GlitchLab/Assets/Scripts/MainMenuManager.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    public Vector3 EndPostition = new Vector3(0, 0, 0);
+    public bool move = false;
+    public GameObject mainMenu;
     public void StartGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        move = true;
+        mainMenu.SetActive(false);
+    }
+    public float speed;
+    void Update()
+    {
+        if (!move) return;
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, EndPostition, step);
+        if (transform.position == EndPostition)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
